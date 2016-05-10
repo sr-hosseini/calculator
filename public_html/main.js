8f0579f984ac735a985bbff847135c75b968e766
Ali Dadashi <alidadashi_1987@yahoo.com>
2016-05-11 00:02:50 +0430

    Most of the features work

--- public_html/main.js
+++ public_html/main.js
@@ -2,24 +2,54 @@
  * Created by Ali on 5/3/2016.
  */
 
-function display(value){
-    var str = value;
-    var patt = new RegExp(/(\d)([-+*\/])(\d)/);
-    var a = str.test(patt);
-    if (!a){
-        form.inputId.value += value;
+//show all clicked item's content in the text box
+
+
+function display(value) {
+    clear();
+    form.inputId.value += value;
+
+}
+
+function calc() {
+
+    reset = true;
+    var reg = /(^(\d+\.{1}\d+)|^(\d+))([-+*\/]{1})((\d+\.{1}\d+)|(\d+))$/g;
+
+    if (reg.test(form.inputId.value)){
+        return eval(form.inputId.value);
     }
     else{
-        form.inputId.value = 'Error!';
+        return 'Invalid Input!';
     }
 }
 
-//^\s*([-+]?)(\d+)(?:\s*([-+*\/])\s*((?:\s[-+])?\d+)\s*)+$
-///^\s*([+-]?(?:\d+\.?\d*|\d*\.\d+))\s*([-+\/*])\s*([+-]?(?:\d+\.?\d*|\d*\.\d+))\s*$/g
-function result(){
-    eval(form.inputId);
+//executed when = in clicked
+
+
+//show or hide the calculator
+function showToggle(){
+    var form = document.getElementById('mainForm');
+    var showHide = form.style.display;
+    var showButton = document.getElementById('showBtn');
+
+    if(showHide == 'block'){
+        //form.style.='2s';
+        form.style.display = 'none';
+        showButton.innerHTML = 'Show Calculator';
+    }
+    else {
+        form.style.display = 'block';
+        showButton.innerHTML = 'Hide Calculator';
+    }
 }
 
 
-
-
+//if textbox is not empty make it empty for the next operation
+var reset = false;
+function clear(){
+    if(reset){
+        form.inputId.value = "";
+    }
+    reset = false;
+}
